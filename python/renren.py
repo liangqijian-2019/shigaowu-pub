9902527324955505863cb0b037d77ab31a30cc49
shigaowu <543152650@qq.com>
2018-12-29 00:00:38 +0800

    403

--- python/renren.py
+++ python/renren.py
@@ -3,8 +3,7 @@ import os
 import re
 import threading
 import urllib.request
-
-COOKIE = 'anonymid=jq7o0wjf-yoc40q; depovince=ZGQT; _r01_=1; ick_login=7bbc73b2-7138-4d72-b635-8a8962478915; first_login_flag=1; ln_uact=shi.gaowu@163.com; ln_hurl=http://hdn.xnimg.cn/photos/hdn521/20121028/1150/h_main_N8xu_096100008de71375.jpg; loginfrom=syshome; ch_id=10016; JSESSIONID=abcnb3OrbxvAMgoBoIZFw; jebe_key=085b9297-b2c9-4f8b-8b05-516f4d1dfbf2%7C1cbb1033eac0de7357478db49946ee23%7C1545979096709%7C1%7C1545979097006; wp_fold=0; jebecookies=c61792eb-a3e5-41a6-abdc-2cc48385981a|||||; _de=0B725AB25083833A855777BB458B199B6DEBB8C2103DE356; p=7b230a7a254bfabc86d814d8f105b6ce3; t=61e38d9685b8d1e63af60fd61c38e6bd3; societyguester=61e38d9685b8d1e63af60fd61c38e6bd3; id=384806863; xnsid=c7369a3'
+COOKIE='anonymid=jq85kkcz-qmozav; depovince=GW; jebecookies=4abe53d0-d4b4-4ad6-9e3e-7db7cfe43164|||||; _r01_=1; ick_login=8be3cb54-c271-4b01-a7d0-7b49c0549957; _de=0B725AB25083833A855777BB458B199B6DEBB8C2103DE356; p=57c81b6e1d6d62d3967e20e81a80862c3; first_login_flag=1; ln_uact=shi.gaowu@163.com; ln_hurl=http://hdn.xnimg.cn/photos/hdn521/20121028/1150/h_main_N8xu_096100008de71375.jpg; t=61e38d9685b8d1e63af60fd61c38e6bd3; societyguester=61e38d9685b8d1e63af60fd61c38e6bd3; id=384806863; xnsid=ab55fdb; ver=7.0; loginfrom=null; jebe_key=a2a2a3bb-b98b-45b3-b5ac-027b1ea5956c%7C1cbb1033eac0de7357478db49946ee23%7C1546008570703%7C1%7C1546008570738; wp_fold=0; XNESSESSIONID=1713c32d95c9; WebOnLineNotice_384806863=1; JSESSIONID=abcnXtMi4A36X6oDct1Fw'
 HEADERS = {'cookie': COOKIE}
 
 
@@ -68,10 +67,10 @@ def find_ablumUrl():
         line = file.readline()
         if line:
             line = line[:-1]
-            photo_url = 'http://photo.renren.com/photo/348359757/album-416572442/v7'
+            photo_url = 'http://photo.renren.com/photo/348359757/album-423181968/v7'
             print(photo_url)
             data = login_renren(photo_url)
-            pattern = re.compile(r'http://photo.renren.com/photo/348359757/albumlist/v7')
+            pattern = re.compile(r'http://photo.renren.com/photo/')
             if pattern.findall(data):
                 list = pattern.findall(data)
             else:
@@ -82,7 +81,7 @@ def find_ablumUrl():
                 albumid_set.add(i)
 
             for i in albumid_set:
-                album_list = 'http://photo.renren.com/photo/348359757/albumlist/v7'
+                album_list = 'http://photo.renren.com/photo/348359757/album-423181968/v7'
                 print(album_list)
                 ablum.write(album_list)
                 ablum.write(os.linesep)
@@ -99,7 +98,7 @@ def download_album():
         else:
             list = ''
             data = login_renren(line)
-            pattern = re.compile(r'large:.*?\.jpg', re.I)  # large xlarge
+            pattern = re.compile(r'http://fmn.rrimg.com/.*?/.*?/original_.*?_.*?\.jpg', re.I)  # large xlarge
             if pattern.findall(data):
                 list = pattern.findall(data)
             else:
@@ -107,7 +106,7 @@ def download_album():
 
             photo_url = set()
             for i in list:
-                i = i[7:]
+                i = i[:]                        #important
                 photo_url.add(i)
                 print(i)  # test
             try:
@@ -128,7 +127,7 @@ class Download(threading.Thread):
     def run(self):
         for i in self.que:
             data = urllib.request.urlopen(i).read()
-            path = str(i[-15:-5]) + '.jpg'
+            path = str(i[:-5]) + '.jpg'
             f = open(path, 'wb')  # 存储下载的图片
             f.write(data)
             f.close()
